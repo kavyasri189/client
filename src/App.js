5221bbf3744b636b61b5cf7b49af1e2aed18b50a
kavyasri189 <64975008+kavyasri189@users.noreply.github.com>
2021-07-14 16:20:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
     }).then((response) => {
       setPasswordList(
         passwordList.map((val) => {
-          return val.id == encryption.id
+          return val.id === encryption.id
             ? {
                 id: val.id,
                 password: val.password,
